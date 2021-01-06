d2a1dc953a488b6e2b185439fbb17ff23dbcefb2
Houda Aynaou <ouda_12@hotmail.fr>
2021-01-05 22:12:54 -0800

    Add login and register

--- application.py
+++ application.py
@@ -33,17 +33,78 @@ def after_request(response):
 def index():
     return render_template('index.html')
 
-@app.route('/get')
-def get():
-    users = User.query.all()
-    return {
-        'users': [{'id':u.id, 'full_name': u.full_name, 'email': u.email} for u in users]
-    }
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/getone')
-def getone():
-    u = User.query.filter_by(full_name='azouz').first()
-    return render_template("user.html", user=u)
+@app.route('/login')
+def login():
+    '''Log user In'''
+    return render_template('login.html')
+
+@app.route('/register' , methods=["GET", "POST"])
+def register():
+    '''Register User'''
+    
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        if request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+            confirmation = request.form.get("confirmation")
+
+        # Validate username
+        if not username or username == '':
+            return "Must provide username"
+
+        # Validate password
+        if not password or password == '':
+            return "Must provide username"
+
+        # Validate password confirmation
+        if not confirmation or confirmation == '':
+            return "Must provide username"
+        # Ensure password and password confirmation match
+        if confirmation != password:
+            return "Must provide username"
+
+        # Ensure username doesn't already taken
+        rows = User.query.filter_by(request.form.get("username"))
+        if len(rows) == 1:
+            return "Username already taken"
+
+        # Create new user
+        
+
+        # Redirect user to login page
+        return redirect("/login")
+
+
+@app.route("/logout")
+def logout():
+    '''Log user out'''
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+
+# @app.route('/get')
+# def get():
+#     users = User.query.all()
+#     return {
+#         'users': [{'id':u.id, 'full_name': u.full_name, 'email': u.email} for u in users]
+#     }
+
+# @app.route('/getone')
+# def getone():
+#     u = User.query.filter_by(full_name='azouz').first()
+#     return render_template("user.html", user=u)
 
 if __name__ == '__main__':
     db.init_app(app)
